b2f8aaa2ec54b01a85948389b7201f5984375151
DANCODEMINER <adchainminer@gmail.com>
2025-07-07 01:05:21 +0100

    Update miner.js

--- static/miner.js
+++ static/miner.js
@@ -73,7 +73,7 @@ function verifyOtp() {
     return;
   }
 
-  fetch("https://danoski-backend.onrender.com/verify-otp", {
+  fetch("https://danoski-backend.onrender.com", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email, otp }),
