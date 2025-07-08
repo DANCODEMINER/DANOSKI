03f6e5cd4c83d7e5541804ea82a931b8cf017616
DANCODEMINER <adchainminer@gmail.com>
2025-07-08 04:53:28 +0100

    Update server.py

--- server.py
+++ server.py
@@ -124,12 +124,14 @@ cur.execute('''
 CREATE TABLE IF NOT EXISTS wallet_settings (
     id SERIAL PRIMARY KEY,
     withdraw_fee_btc NUMERIC DEFAULT 0
+    
 );
 ''')
 
     conn.commit()
     cur.close()
     conn.close()
+
 
 # === UTILITIES ===
 
